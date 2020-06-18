3e0a87bf1314a51f634aac088e8cd6fbd8294e54
Hideyuki191 <hideyuki1000000@gmail.com>
2020-06-18 16:05:59 +0800

    Integrated the files into a unity project

---
+++ 31_HideyukiAtago_PA11/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    public static int points = 0;
+
+    public Text Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = "Score: " + points;
+
+        RestrictMovement();
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+    }
+
+    private void RestrictMovement()
+    {
+        if(transform.position.y >= 3.93f)
+        {
+            gameObject.transform.position = new Vector3(-4.28f, 3.93f, 0.008284654f);
+        }
+
+        if(transform.position.y <= -3.9f)
+        {
+            gameObject.transform.position = new Vector3(-4.28f, -3.9f, 0.008284654f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            points = 0;
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
